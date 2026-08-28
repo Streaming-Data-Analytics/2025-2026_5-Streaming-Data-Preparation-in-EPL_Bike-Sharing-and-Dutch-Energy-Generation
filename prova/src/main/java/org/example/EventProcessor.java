5b96f63e9b1f1dbbee043b94850080ef17c6edcd
AndreaLancini <10707309@polimi.it>
2026-08-28 17:29:41 +0200

    Main files of the project

---
+++ prova/src/main/java/org/example/EventProcessor.java
@@ -0,0 +1,109 @@
+package org.example;
+
+import com.espertech.esper.common.client.EPCompiled;
+import com.espertech.esper.common.client.configuration.Configuration;
+import com.espertech.esper.compiler.client.CompilerArguments;
+import com.espertech.esper.compiler.client.EPCompileException;
+import com.espertech.esper.compiler.client.EPCompiler;
+import com.espertech.esper.compiler.client.EPCompilerProvider;
+import com.espertech.esper.runtime.client.*;
+import com.espertech.esperio.csv.AdapterInputSource;
+import com.espertech.esperio.csv.CSVInputAdapter;
+
+import java.io.File;
+import java.util.Map;
+
+public class EventProcessor {
+    private File csvFile;
+    private String eventName;
+    private Configuration configuration;
+    private EPRuntime runtime;
+    private EPStatement statement;
+
+    //Initialize the Event Processing Runtime using the filepath and the structure of the event
+    public EventProcessor(String filePath, String eventName, Map<String, Object> eventProperties) {
+        try {
+            this.csvFile = new File(filePath);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        this.eventName = eventName;
+        this.configuration = new Configuration();
+        this.configuration.getCommon().addEventType(eventName, eventProperties);
+        this.runtime = EPRuntimeProvider.getDefaultRuntime(this.configuration);
+        this.statement = null;
+
+        this.configuration.getCommon().addImport("java.time.*");
+
+    }
+
+    //Change the configuration of the EP runtime
+    public void changeConfiguration(String eventName, Map<String, Object> eventProperties) {
+        this.eventName = eventName;
+        this.configuration = new Configuration();
+        this.configuration.getCommon().addEventType(eventName, eventProperties);
+        this.runtime = EPRuntimeProvider.getDefaultRuntime(this.configuration);
+    }
+
+    public Configuration getConfiguration() {
+        return configuration;
+    }
+
+    public EPRuntime getRuntime() {
+        return runtime;
+    }
+
+    public File getCsvFile() {
+        return csvFile;
+    }
+
+    public void setCsvFile(String filePath) {
+        File newFile;
+        try{
+            newFile = new File(filePath);
+            this.csvFile = newFile;
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public EPStatement getStatement() {
+        return statement;
+    }
+
+    //Compile and deploy an EPL query
+    public void compileDeploy(String epl) {
+        try {
+            // Get the compiler
+            EPCompiler compiler = EPCompilerProvider.getCompiler();
+
+            // Pass our configuration to the compiler arguments
+            CompilerArguments args = new CompilerArguments(this.configuration);
+
+            // Compile the string into bytecode
+            EPCompiled compiled = compiler.compile(epl, args);
+
+            // Deploy the bytecode to the runtime
+            EPDeployment deployment = this.runtime.getDeploymentService().deploy(compiled);
+
+            // Return the first statement in the deployment
+            int totalStatements = deployment.getStatements().length;
+            if(totalStatements > 0)
+                this.statement =  deployment.getStatements()[totalStatements-1];
+
+        } catch (EPCompileException | EPDeployException e) {
+            throw new RuntimeException("Failed to compile or deploy EPL: " + e.getMessage(), e);
+        }
+    }
+
+    //Add a listener to the event stream to collect data
+    public void addListener(UpdateListener listener) {
+        statement.addListener(listener);
+    }
+
+    //Start the stream of events
+    public void startStream(String name){
+        (new CSVInputAdapter(runtime, new AdapterInputSource(csvFile), name)).start();
+    }
+}
